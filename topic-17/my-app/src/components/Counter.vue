3d76386fc9aabf7113f3c92e3a4941c26adc0b91
anilsuwal <anilsuwal58@gmail.com>
2020-08-12 22:35:47 +0545

    chapter 16 17 completed

---
+++ topic-17/my-app/src/components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>The Count is: {{ count }}</h2>
+
+        <p>
+            <button @click="count++">Increment</button>
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {count: 0};
+    }
+}
+</script>
